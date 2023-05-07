17ee3af58601082fa6cc3bcbc0fcdf01540f8c50
Rodolfo Gomes <rodolfogf3@gmail.com>
2023-05-07 15:47:44 -0300

    Configuracoes de rota

--- src/ForLogic.GatewayAPI/Program.cs
+++ src/ForLogic.GatewayAPI/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
 namespace ForLogic.GatewayAPI
 {
     public class Program
@@ -7,6 +12,17 @@ namespace ForLogic.GatewayAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            //builder.Services.AddAuthentication("Bearer")
+            //    .AddJwtBearer("Bearer", options =>
+            //    {
+            //        options.Authority = "https://localhost:4435/";
+            //        options.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidateAudience = false
+            //        };
+            //    });
+
+            builder.Services.AddOcelot();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,14 +38,7 @@ namespace ForLogic.GatewayAPI
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseOcelot();
         }
     }
 }
